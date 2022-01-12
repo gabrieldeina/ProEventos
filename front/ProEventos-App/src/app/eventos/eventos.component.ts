5a6f03d86989e5c8312e0e477120013dc570970c
Gabriel Deina <gabrieldeina@Gabriels-MacBook-Pro.local>
2022-01-11 23:55:20 -0300

    section five

--- front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
-  public eventos: any;
+  eventos: any = [];
+  eventosFiltrados: any = [];
+  isCollapsed = true;
+  private _filtroLista = '';
 
   constructor(private readonly http: HttpClient) {}
 
@@ -15,26 +18,34 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  public get filtroLista(): string {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(search: string) {
+    this._filtroLista = search;
+    this.eventosFiltrados = this.filtroLista
+      ? this.filtrarEventos(this.filtroLista)
+      : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLowerCase();
+    return this.eventos.filter(
+      (evento: any) =>
+        evento.tema.toLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.local.toLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
+
   getEventos() {
     this.http.get('https://localhost:7102/api/Evento').subscribe({
-      next: (response) => this.eventos = response,
+      next: (response) => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
       error: (err) => console.error(err),
-      complete: () => console.info('getEventos complete')
-  })
-
-    this.eventos = [
-      {
-        tema: 'Angular',
-        local: 'Belo Horizonrte',
-      },
-      {
-        tema: 'Node',
-        local: 'São Paulo',
-      },
-      {
-        tema: '.NET',
-        local: 'Curitiba',
-      },
-    ];
+      complete: () => console.info('getEventos complete'),
+    });
   }
 }
